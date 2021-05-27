33705466be25b7e0addcf6d71641fa0d4c565376
Simon Widmann <64704230+Widdi97@users.noreply.github.com>
2021-05-27 18:37:11 +0200

    1

--- Eigenspektrum_SSH_model.py
+++ Eigenspektrum_SSH_model.py
@@ -1,16 +1,5 @@
 import numpy as np
-try:
-    import cupy as cp  # effiziernte Eigenwertberechnung
-except:
-    class cupyPlaceholder:
-        def __init__(self):
-            pass
-        
-        def asnumpy(self,x):
-            return x
-    cp = cupyPlaceholder()
 import matplotlib.pyplot as plt
-import time
 
 
 beta = 1 # inter-cell coupling
@@ -20,14 +9,8 @@ steps = 2000 # resolution
 v_list = [ 0.001 + 3*1/steps*k for k in range(steps+1)] # bei v=0: Err, da degenerierte EW
 graphs = [ [] for k in range(2*N) ]
 
-useGpuAccel = False
+H = np.zeros(( 2*N,2*N ))
 
-if useGpuAccel:
-    H = cp.zeros( ( 2*N,2*N ) )
-else:
-    H = np.zeros(( 2*N,2*N ))
-
-t0 = time.time()
 
 #populate H:
 for alpha in v_list:
@@ -40,15 +23,11 @@ for alpha in v_list:
     H[-1,0] = 0
     H[0,-1] = 0
 
-    if useGpuAccel:
-        energies = cp.linalg.eigvalsh(H)
-    else:
-        energies = np.linalg.eigvalsh(H)#[ sp.re(key.evalf()) for key in H.eigenvals().keys() ] # realteil, da floatingpoint errors zu kleinen imaginären Anteilen führen
+    energies = np.linalg.eigvalsh(H)#[ sp.re(key.evalf()) for key in H.eigenvals().keys() ] # realteil, da floatingpoint errors zu kleinen imaginären Anteilen führen
     energies.sort()
     for k in range(2*N):
         graphs[k].append(energies[k])
-        
-print("calculation time: ",time.time()-t0,"s")
+
 
 fig, ax = plt.subplots(figsize=(6,4.5))
 for k in range(2*N):
@@ -60,5 +39,4 @@ plt.ylabel("Energy $E$")
 plt.text(0.2, -2.6, '$β={}$'.format(beta))
 plt.vlines(1,-3,3,color="grey",linestyle="-.",linewidth=0.5)
 fig.tight_layout()
-# plt.savefig("spectrum.pdf")
 plt.show()
